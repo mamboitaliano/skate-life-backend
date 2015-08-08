58393f87bd3e2195ecd9a4a94c6951a0af168197
Chris Scott <christo247@gmail.com>
2015-08-08 15:10:53 -0700

    API now passes hash with user and user skateparks for user show route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,7 +16,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     # render json: @user
-    render json: {@user, @user.skateparks}
+    render json: {user: @user, skateparks: @user.skateparks}
   end
 
   def destroy
