3940f16f3d13e4d7c0b9f97e710cd4f25874045c
Shahab Amin <amins.shahab@gmail.com>
2015-08-13 19:54:14 -0700

    reseeding with Johns data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,58 +1,59 @@
-# require 'csv'
-# require 'geokit'
+require 'csv'
+require 'geokit'
 
-# CSV.foreach('db/skateparks.csv', headers: true, header_converters: :symbol, ) do |row|
-#   park_params = Hash[row]
+CSV.foreach('db/skateparks.csv', headers: true, header_converters: :symbol, ) do |row|
+  park_params = Hash[row]
 
-#   if park_params[:address]
-#     sleep(0.2)
-#     lat_long = Geokit::Geocoders::GoogleGeocoder.geocode(park_params[:address]) if park_params[:address]
-#     lat = lat_long.to_s.match(/Latitude: (.\d+\.\d+)/)[1]
-#     long = lat_long.to_s.match(/Longitude: (.\d+\.\d+)/)[1]
+  if park_params[:address]
+    sleep(0.2)
+    lat_long = Geokit::Geocoders::GoogleGeocoder.geocode(park_params[:address]) if park_params[:address]
+    lat = lat_long.to_s.match(/Latitude: (.\d+\.\d+)/)[1]
+    long = lat_long.to_s.match(/Longitude: (.\d+\.\d+)/)[1]
 
-#     if park_params[:name]
-#       Skatepark.create(
-#         name: park_params[:name],
-#         address: park_params[:address],
-#         lat: lat,
-#         lon: long)
-#     else
-#       Skatepark.create(
-#         name: park_params[:city],
-#         address: park_params[:address],
-#         lat: lat,
-#         lon: long)
-#     end
-#   end
-
-# end
-
-
-
-
-require 'nokogiri'
-
-@doc = Nokogiri::XML(File.open('db/skateparks.kml'))
-
-@doc.css('Placemark').each do |placemark|
-  name = placemark.css('name')
-  coordinates = placemark.at_css('coordinates')
-  address_reg_exp = placemark.at_css('description').to_s.match(/(\d+\ \w+\ \w+)<br>(\w+, \w+\ \d+)/)
-
-  address = $1 + $2 if address_reg_exp
-
-  if name && coordinates
-    # print name.text + ","
-    coordinates.text.split(' ').each do |coordinate|
-      (lon,lat,elevation) = coordinate.split(',')
+    if park_params[:name]
       Skatepark.create(
-        name: name.text,
-        address: address,
+        name: park_params[:name],
+        address: park_params[:address],
         lat: lat,
-        lon: lon)
+        lon: long)
+    else
+      Skatepark.create(
+        name: park_params[:city],
+        address: park_params[:address],
+        lat: lat,
+        lon: long)
     end
   end
+
 end
+
+
+
+
+# require 'nokogiri'
+
+# @doc = Nokogiri::XML(File.open('db/skateparks.kml'))
+
+# @doc.css('Placemark').each do |placemark|
+#   name = placemark.css('name')
+#   coordinates = placemark.at_css('coordinates')
+#   address_reg_exp = placemark.at_css('description').to_s.match(/(\d+\ \w+\ \w+)<br>(\w+, \w+\ \d+)/)
+
+#   address = $1 + $2 if address_reg_exp
+#   # p address
+
+#   if name && coordinates
+#     # print name.text + ","
+#     coordinates.text.split(' ').each do |coordinate|
+#       (lon,lat,elevation) = coordinate.split(',')
+#       Skatepark.create(
+#         name: name.text,
+#         address: address,
+#         lat: lat,
+#         lon: lon)
+#     end
+#   end
+# end
 
 
 # # LOCAL SEED
